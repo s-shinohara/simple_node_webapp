e1e13e76aad50ab9e0a52d2d5b030017a0ae4a8e
Shingo Shinohara <sshinohara@happoh-systems.net>
2015-08-22 19:02:50 +0900

    いろいろ書いた

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 8080;
 //Webサーバーの設定
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Worldああああ\n');
+  res.end('Hello World篠原のでjs\n');
 }).listen(port);
 
 console.log('Server running at http://localhost:3000/');
