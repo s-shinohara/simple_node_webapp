87da78f38454885be1acaa1ed23e4cf9a0e2174b
Shingo Shinohara <sshinohara@happoh-systems.net>
2015-08-22 18:56:45 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+//httpモジュールをインポート
+var http = require('http');
+
+var port = process.env.PORT || 8080;
+
+//Webサーバーの設定
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Worldああああ\n');
+}).listen(port);
+
+console.log('Server running at http://localhost:3000/');
